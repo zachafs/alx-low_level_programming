e58cc27e2da9194979aa6b597af44afc50fb431a
zachafs <ommaratz@gmail.com>
2023-03-18 10:38:34 -0700

    this the end project c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+* main - UNIX is basically a simple operating system
+** Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+fprintf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+return (1);
+}
+
