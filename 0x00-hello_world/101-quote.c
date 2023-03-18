17f8865722862edbd9ca27a39ecdbad1b4774c71
zachafs <ommaratz@gmail.com>
2023-03-18 11:05:16 -0700

    no way i done my first project c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
 * main - UNIX is basically a simple operating system
 ** Return: Always 0 (Success)
@@ -6,7 +7,7 @@
 
 int main(void)
 {
-fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stderr);
 return (1);
 }
 
