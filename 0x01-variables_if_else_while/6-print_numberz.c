9c025ee78bf3d5e7649a12cb6a27075d82833142
zachafs <ommaratz@gmail.com>
2023-03-20 08:27:27 -0700

    the source of number

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+
+	n = 0;
+	while (n < 10)
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+return (0);
+}
