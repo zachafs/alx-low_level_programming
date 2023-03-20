93a4b92a6721069bc366e4aaa5034db159af40b1
zachafs <ommaratz@gmail.com>
2023-03-20 08:15:02 -0700

    this is the number c program

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+
+/**
+ * main - numberz
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n = 0;
+
+	while (n < 10)
+	{
+		printf("%d", n);
+		n++;
+	}
+	printf("\n");
+return (0);
+}
