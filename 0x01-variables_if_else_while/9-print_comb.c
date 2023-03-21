8faa7ee4405ebdafa0950fc6194ec00410db1338
zachafs <ommaratz@gmail.com>
2023-03-21 06:58:07 -0700

    task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -2,7 +2,8 @@
 
 
 /**
- * main - ite a program that prints all possible combinations of single-digit numbers.
+ * main - ite a program that prints all
+ * possible combinations of single-digit numbers.
  *
  * Return: Always 0 (Success)
  */
@@ -10,14 +11,17 @@
 int main(void)
 {
 	int a = 0;
-	
-	while (a < 10)
-		{
-			putchar(' ');
-			putchar(a +  '0');
-			putchar(',');
-			a++;
-		}
-	putchar('\n');
-	return (0);
+
+while (a < 10)
+{
+putchar(' ');
+putchar(a +  '0');
+if (a < 9)
+{
+putchar(',');
+}
+a++;
+}
+putchar('\n');
+return (0);
 }
