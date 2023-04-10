99101b1a8017d4e9b369d4d18f79e14e8626e813
zachafs <ommaratz@gmail.com>
2023-04-10 08:42:58 -0700

    this is staic putchar

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,14 +1,16 @@
-#include "main.h"
-void _puts(char *str)
 #include<stdio.h>
+#include "main.h"
+
+void _puts(char *str)
+{
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
+}
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
-
