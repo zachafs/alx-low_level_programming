8c43b8bb2fb6573ff74637cc8799514dfb56f827
zachafs <ommaratz@gmail.com>
2023-04-12 06:37:58 -0700

    this is it

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,29 +1,36 @@
 #include <stdlib.h>
 #include "main.h"
+
 /**
- *  * count_word - helper function to count the number of words in a string
- *  @s: string to evaluat
- *   Return: number of words
+ * count_word - helper function to count the number of words in a string
+ * @s: string to evaluate
+ *
+ * Return: number of words
  */
 int count_word(char *s)
 {
-int flag, c, w;
+	int flag, c, w;
 
-flag = 0;
+	flag = 0;
 	w = 0;
+
 	for (c = 0; s[c] != '\0'; c++)
 	{
 		if (s[c] == ' ')
 			flag = 0;
-		else if (flag == 0)
-			flag = 1;
-		w++;
-						}
-		return (w);
+else if (flag == 0)
+		{
+flag = 1;
+			w++;
+		}
+	}
+
+	return (w);
 }
 /**
  * **strtow - splits a string into words
  * @str: string to split
+ *
  * Return: pointer to an array of strings (Success)
  * or NULL (Error)
  */
@@ -37,9 +44,11 @@ char **strtow(char *str)
 	words = count_word(str);
 	if (words == 0)
 		return (NULL);
+
 	matrix = (char **) malloc(sizeof(char *) * (words + 1));
 	if (matrix == NULL)
 		return (NULL);
+
 	for (i = 0; i <= len; i++)
 	{
 		if (str[i] == ' ' || str[i] == '\0')
@@ -50,8 +59,9 @@ char **strtow(char *str)
 				tmp = (char *) malloc(sizeof(char) * (c + 1));
 				if (tmp == NULL)
 					return (NULL);
+
 				while (start < end)
-					*tmp++ = str[start++];
+				*tmp++ = str[start++];
 				*tmp = '\0';
 				matrix[k] = tmp - c;
 				k++;
@@ -61,7 +71,9 @@ char **strtow(char *str)
 		else if (c++ == 0)
 			start = i;
 	}
+
 	matrix[k] = NULL;
+
 	return (matrix);
 }
 
